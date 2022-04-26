298197995a56ff3ebed67424a1e731b9992de1fc
Leon Seipp <l.seipp@browserwerk.de>
2022-04-26 13:59:00 +0200

    Trying to get TER publish to work

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,30 +1,21 @@
 <?php
-
-declare(strict_types=1);
-
-/**
- * This file is part of the "BW Jobs" Extension for TYPO3 CMS.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * (c) 2022 Leon Seipp <l.seipp@browserwerk.de>, Browserwerk GmbH
- */
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'BW Jobs',
     'description' => 'A job board extension for TYPO3 CMS.',
     'category' => 'plugin',
-    'author' => 'Browserwerk GmbH',
+    'author' => 'Browserwerk Team',
     'author_email' => 'info@browserwerk.de',
+    'author_company' => 'Browserwerk GmbH',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
     'version' => '2.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '11.5.0-11.99.99',
         ],
-        'conflicts' => [],
-        'suggests' => [],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
     ],
 ];
